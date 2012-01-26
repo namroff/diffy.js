92afd24b262f43331988767a9dc8849ce2734a51
Nate Forman <nate@nateforman.com>
2012-01-25 23:57:03 -0600

    refresh
    - better versions of impl and test from another branch
    - added iffe and hooks for node and require

--- diffy.js
+++ diffy.js
@@ -1,65 +1,90 @@
-    function diffy(expected, actual, alreadyProcessed) {
+(function( root ) {
 
-      var myExpected  = _.clone(expected),
-          myActual    = _.clone(actual),
-          myProcessed = alreadyProcessed || [],
-          same = false,
-          subcompare,
-          key;
+  var diffy = function diffy( expected, actual, alreadyProcessed ) {
 
-      // TODO: something better with functions?
-      if ((typeof myExpected) == (typeof myActual)) {
-        if (_.isArray(myExpected) && _.isArray(myActual)) {
-          // compare each element
-          same = true; // initialization
-          _(myExpected).each(function(expectedValue, index) {
-            subcompare = diffy(expectedValue, myActual[index], myProcessed);
-            if (subcompare.same) {
-              myExpected[index] = undefined;
-              if (index < myActual.length) {
-                myActual[index] = undefined;
-              }
-            } else {
-              myExpected[index] = subcompare.expected;
-              if (index < myActual.length) {
-                myActual[index] = subcompare.actual;
-              }
+    var myExpected  = _.clone( expected ),
+        myActual    = _.clone( actual ),
+        myProcessed = alreadyProcessed || [],
+        same = false,
+        subcompare,
+        key;
+
+    // TODO: something better with functions?
+    if ( (typeof myExpected) == (typeof myActual) ) {
+      if ( _.isArray( myExpected ) && _.isArray( myActual ) ) {
+        // compare each element
+        same = true; // initialization
+        _( myExpected ).each(function ( expectedValue, index ) {
+          subcompare = diffy( expectedValue, myActual[ index ], myProcessed );
+          if ( subcompare.same ) {
+            myExpected[ index ] = undefined;
+            if ( index < myActual.length ) {
+              myActual[ index ] = undefined;
             }
-            same = same && subcompare.same;
-          });
-        } 
-        else if (typeof myExpected == 'object') {
-          if ((_.indexOf(myProcessed, myExpected) == -1) &&
-              (_.indexOf(myProcessed, myActual) == -1)) {
-            myProcessed.push(myExpected);
-            myProcessed.push(myActual);
-            // compare each key's value
-            _(myExpected).each(function(expectedValue, key) {
-              if (myExpected.hasOwnProperty(key)) {
-                subcompare = diffy(expectedValue, myActual[key], myProcessed);
-                if (subcompare.same) {
-                  delete myExpected[key];
-                  delete myActual[key];
-                } else {
-                  myExpected[key] = subcompare.expected;
-                  if (myActual.hasOwnProperty(key)) {
-                    myActual[key] = subcompare.actual;
-                  }
+          } 
+          else {
+            myExpected[ index ] = subcompare.expected;
+            if ( index < myActual.length ) {
+              myActual[ index ] = subcompare.actual;
+            }
+          }
+          same = same && subcompare.same;
+        });
+      } 
+      else if ( typeof myExpected == 'object' ) {
+        if ( (_.indexOf( myProcessed, myExpected ) == -1) &&
+             (_.indexOf( myProcessed, myActual ) == -1) ) {
+
+          myProcessed.push( myExpected );
+          myProcessed.push( myActual );
+
+          // compare each key's value
+          _( myExpected ).each(function ( expectedValue, key ) {
+            if ( myExpected.hasOwnProperty( key ) ) {
+              subcompare = diffy( expectedValue, myActual[ key ], myProcessed );
+              if ( subcompare.same ) {
+                delete myExpected[ key ];
+                delete myActual[ key ];
+              } 
+              else {
+                myExpected[ key ] = subcompare.expected;
+                if ( myActual.hasOwnProperty( key ) ) {
+                  myActual[ key ] = subcompare.actual;
                 }
               }
-            });
-          }
-          same = _.isEmpty(myExpected) && _.isEmpty(myActual);
-        } 
-        else {
-          // straight comparison
-          same = (myExpected === myActual);
+            }
+          });
         }
-      } // else leave them--they're different
+        same = _.isEmpty( myExpected ) && _.isEmpty( myActual );
+      } 
+      else {
+        // straight comparison
+        same = ( myExpected === myActual );
+      }
+    } // else leave them--they're different
 
-      return {
-        'same': same,
-        'expected': myExpected,
-        'actual':   myActual
-      };
+    return {
+      'same': same,
+      'expected': myExpected,
+      'actual':   myActual
+    };
+  };
+
+  // Handle node, amd, and global systems
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = diffy;
     }
+    exports.diffy = diffy;
+  }
+  else {
+    if (typeof define === 'function' && define.amd) {
+      define('diffy', function() {
+        return diffy;
+      });
+    }
+    // Leak a global regardless of module system
+    root['diffy'] = diffy;
+  }
+
+})( this );
--- test/tests.js
+++ test/tests.js
@@ -1,80 +1,91 @@
-    it ('diffy number test', function() {
-      var result = diffy(1,1);
-      expect(result.same).to.be.ok();
-      expect(result.expected).to.be(1);
-      expect(result.actual).to.be(1);
-      result = diffy(2,1);
+describe( 'diffy.js tests', function () {
+
+    it ( 'diffy number test', function () {
+
+      var result = diffy( 1, 1 );
+      expect( result.same ).to.be.ok();
+      expect( result.expected ).to.be( 1 );
+      expect( result.actual ).to.be( 1 );
+
+      result = diffy( 2, 1 );
       expect(result.same).to.not.be.ok();
-      expect(result.expected).to.be(2);
-      expect(result.actual).to.be(1);
+      expect(result.expected).to.be( 2 );
+      expect(result.actual).to.be( 1 );
     });
 
-    it ('diffy string test', function() {
-      var result = diffy('foo', 'foo');
-      expect(result.same).to.be.ok();
-      expect(result.expected).to.be('foo');
-      expect(result.actual).to.be('foo');
-      result = diffy('foo', 'bar');
-      expect(result.same).to.not.be.ok();
-      expect(result.expected).to.be('foo');
-      expect(result.actual).to.be('bar');
+    it ( 'diffy string test', function () {
+
+      var result = diffy( 'foo', 'foo' );
+      expect( result.same ).to.be.ok();
+      expect( result.expected ).to.be( 'foo' );
+      expect( result.actual ).to.be( 'foo' );
+
+      result = diffy( 'foo', 'bar' );
+      expect( result.same ).to.not.be.ok();
+      expect( result.expected ).to.be( 'foo' );
+      expect( result.actual ).to.be( 'bar' );
     });
 
     var object1 = {
       foo: 1,
       bar: "baz"
     };
+
     var object2 = {
       foo: 1,
       bar: "baz"
     };
+
     var object3 = {
       foo: 1,
       bar: "baz",
       extra: null
     };
+
     var object4 = {
       foo: 1
     };
+
     var object5 = {
       foo: "apple",
       bar: "baz"
     };
 
-    it ('diffy object test', function() {
-      var result = diffy(object1, object2);
-      expect(result.same).to.be.ok();
-      expect(_.isEmpty(result.expected)).to.be.ok();
-      expect(_.isEmpty(result.actual)).to.be.ok();
+    it ( 'diffy object test', function () {
+      var result = diffy( object1, object2 );
+      expect( result.same ).to.be.ok();
+      expect( _.isEmpty( result.expected ) ).to.be.ok();
+      expect( _.isEmpty( result.actual ) ).to.be.ok();
 
-      result = diffy(object1, object3);
-      expect(result.same).to.not.be.ok();
-      expect(_.isEmpty(result.expected)).to.be.ok();
-      expect(result.actual).to.eql({extra:null});
+      result = diffy( object1, object3 );
+      expect( result.same ).to.not.be.ok();
+      expect( _.isEmpty( result.expected ) ).to.be.ok();
+      expect( result.actual ).to.eql( { extra: null } );
 
-      result = diffy(object1, object4);
-      expect(result.same).to.not.be.ok();
-      expect(result.expected).to.eql({bar:"baz"});
-      expect(_.isEmpty(result.actual)).to.be.ok();
+      result = diffy( object1, object4 );
+      expect( result.same ).to.not.be.ok();
+      expect( result.expected ).to.eql( { bar: "baz" } );
+      expect( _.isEmpty( result.actual ) ).to.be.ok();
 
-      result = diffy(object1, object5);
-      expect(result.same).to.not.be.ok();
-      expect(result.expected).to.eql({foo:1});
-      expect(result.actual).to.eql({foo:'apple'});
+      result = diffy( object1, object5 );
+      expect( result.same ).to.not.be.ok();
+      expect( result.expected ).to.eql( { foo: 1 } );
+      expect( result.actual ).to.eql( { foo: 'apple' } );
     });
 
-    it ('diffy array test', function() {
-      var result = diffy(['foo', 3, null], ['foo', 3, null]);
-      expect(result.same).to.be.ok();
-      expect(result.expected).to.eql([ undefined, undefined, undefined ]);
-      expect(result.actual).to.eql([ undefined, undefined, undefined ]);
-      result = diffy(['foo', 3, null], ['foo', 3, 'apple']);
-      expect(result.same).to.not.be.ok();
-      expect(result.expected).to.eql([undefined, undefined, null]);
-      expect(result.actual).to.eql([undefined, undefined, 'apple']);
+    it ( 'diffy array test', function () {
+      var result = diffy( [ 'foo', 3, null ], [ 'foo', 3, null ] );
+      expect( result.same ).to.be.ok();
+      expect( result.expected ).to.eql([ undefined, undefined, undefined ]);
+      expect( result.actual ).to.eql([ undefined, undefined, undefined ]);
+
+      result = diffy( [ 'foo', 3, null ], [ 'foo', 3, 'apple' ] );
+      expect( result.same ).to.not.be.ok();
+      expect( result.expected ).to.eql([ undefined, undefined, null ]);
+      expect( result.actual ).to.eql([ undefined, undefined, 'apple' ]);
     });
 
-    it ('diffy does not hose nested objects', function() {
+    it ( 'diffy does not hose nested objects', function () {
       var obj1 = {
           "SecondaryRatings": {
             "Value": {
@@ -123,12 +134,12 @@
             }
           }
         };
-      var result = diffy(obj1, obj2);
-      expect(result.same).to.be.ok();
-      expect(_.isEmpty(result.expected)).to.be.ok();
-      expect(_.isEmpty(result.actual)).to.be.ok();
-      expect(_.isEmpty(obj1.SecondaryRatings.Value)).to.not.be.ok();
-      expect(_.isEmpty(obj2.SecondaryRatings.Value)).to.not.be.ok();
+      var result = diffy( obj1, obj2 );
+      expect( result.same ).to.be.ok();
+      expect( _.isEmpty( result.expected ) ).to.be.ok();
+      expect( _.isEmpty( result.actual ) ).to.be.ok();
+      expect( _.isEmpty( obj1.SecondaryRatings.Value ) ).to.not.be.ok();
+      expect( _.isEmpty( obj2.SecondaryRatings.Value ) ).to.not.be.ok();
     });
 
     it ('diffy trivial cycle test' // TODO: PENDING--terminates, but yields wrong answer
@@ -137,18 +148,20 @@
     //       obj2 = {};
     //   obj1.self = obj1;
     //   obj2.self = obj2;
-    //   var result = diffy(obj1, obj2);
-    //   expect(result.same).to.be.ok();
-    //   expect(_.isEmpty(result.expected)).to.be.ok();
-    //   expect(_.isEmpty(result.actual)).to.be.ok();
+    //   var result = diffy( obj1, obj2 );
+    //   expect( result.same ).to.be.ok();
+    //   expect( _.isEmpty( result.expected ) ).to.be.ok();
+    //   expect( _.isEmpty( result.actual ) ).to.be.ok();
     // }
     );
 
     it('faq test', function() {
       // NF: demonstrates that underscore.indexOf() operates on ===
       // var obj = {};
-      // var arr = [obj];
-      // var other = [{}];
-      // expect(_.indexOf(arr, obj)).to.be(0);
-      // expect(_.indexOf(other, obj)).to.be(-1);
+      // var arr = [ obj ];
+      // var other = [ {} ];
+      // expect( _.indexOf( arr, obj ) ).to.be( 0 );
+      // expect( _.indexOf( other, obj ) ).to.be( -1 );
     });
+  });
+});
